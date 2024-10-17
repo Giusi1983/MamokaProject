5a87af1acf9895b0ea90679df93e2433e71c72b7
Giusi1983 <68873947+Giusi1983@users.noreply.github.com>
2024-10-17 07:23:47 +0200

    Added design to login

--- movie-app/src/components/Login.vue
+++ movie-app/src/components/Login.vue
@@ -1,17 +1,42 @@
 <template>
-  <div class="login-container">
-    <h1>Login</h1>
-    <form @submit.prevent="handleLogin">
-      <div>
-        <label for="username">Nome Utente</label>
-        <input v-model="username" id="username" type="text" required />
-      </div>
-      <div>
-        <label for="password">Password</label>
-        <input v-model="password" id="password" type="password" required />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div class="bg-white p-6 rounded-lg shadow-lg max-w-sm min-w-full">
+      <h1 class="text-2xl font-bold mb-4 text-center">Login</h1>
+      <form @submit.prevent="handleLogin">
+        <div class="mb-4">
+          <label for="username" class="block text-sm font-medium text"
+            >Nome Utente</label
+          >
+          <input
+            v-model="username"
+            id="username"
+            type="text"
+            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            required
+          />
+        </div>
+        <div class="mb-4">
+          <label
+            for="password"
+            class="block text-sm font-medium text-gray-700 mb-2"
+            >Password</label
+          >
+          <input
+            v-model="password"
+            id="password"
+            type="password"
+            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+        >
+          Login
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
